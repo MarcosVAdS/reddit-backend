71f3439df21fc215df3d3187487122ee35535a6b
itsmath <matheuscardhosoo@gmail.com>
2020-12-15 14:34:53 -0300

    Adjust S3 helper to use id

--- helpers/s3.py
+++ helpers/s3.py
@@ -14,15 +14,15 @@ from django.utils import timezone
 # Upload images to S3 storage
 ###
 @deconstructible
-class UploadFileTo(object):
+class UploadFileTo():
     def __init__(self, folder, suffix):
         self.folder = folder
         self.suffix = suffix
 
     def __call__(self, instance, filename):
-        filename_base, filename_ext = os.path.splitext(filename)
+        _, filename_ext = os.path.splitext(filename)
         return '{0}/{1}{2}'.format(
             self.folder,
-            '{0}-{1}-{2}'.format(instance.uuid, self.suffix, timezone.now().isoformat()),
+            '{0}-{1}-{2}'.format(instance.id, self.suffix, timezone.now().isoformat()),
             filename_ext.lower(),
         )
